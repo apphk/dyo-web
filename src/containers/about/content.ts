c374488334198104fed83115b5f00dc7f7ad1d33
Kato Yeung <katoyeung@gmail.com>
2021-07-04 10:07:06 +0800

    first commit

---
+++ src/containers/about/content.ts
@@ -0,0 +1,16 @@
+import { FaGithub } from 'react-icons/fa';
+
+const content = {
+  title: 'About.',
+  subtitle: 'Find out more on Github',
+  links: [
+    {
+      to: 'https://github.com/apphk',
+      text: 'apphk',
+      Icon: FaGithub,
+      newTab: true,
+    },
+  ],
+};
+
+export default content;
---
+++ src/containers/welcome/content.ts
@@ -0,0 +1,7 @@
+const content = {
+  tagline: 'Design & Develop Your Own Application',
+  title: 'DYO APP',
+  subtitle: 'Solution Architect, Tailor-Made System, Web & Mobile App.',
+};
+
+export default content;
